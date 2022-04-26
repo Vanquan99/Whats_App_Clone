18cc9129322baf6002a509079ca86ab45a4dd493
vanquan99 <vanquanqn99@gmail.com>
2022-04-26 15:35:26 +0700

    logout

--- app/src/main/java/com/vanquan/whatsappclone/MainActivity.java
+++ app/src/main/java/com/vanquan/whatsappclone/MainActivity.java
@@ -1,14 +1,59 @@
 package com.vanquan.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.vanquan.whatsappclone.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+
+        getSupportActionBar().hide();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.setting:
+                Toast.makeText(this, "Clicked menu", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.logout:
+                auth.signOut();
+
+                Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
